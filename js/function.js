5cb15d67b333f552a9bc75b28d7335a47b018a38
hokilehi <hokilehi@gmail.com>
2023-03-26 19:02:23 -0300

    2

---
+++ js/function.js
@@ -0,0 +1,26 @@
+function pulsarboton1() {
+    var x = document.getElementById("sobremi");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function pulsarboton2() {
+    var x = document.getElementById("habilidades");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function pulsarboton3() {
+    var x = document.getElementById("contacto");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
