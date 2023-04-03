1083e0905732905f6732dc2cc2bed30a2c0e1ad2
hokilehi <hokilehi@gmail.com>
2023-04-03 09:01:35 -0300

    htm css js completo

---
+++ function.js
@@ -0,0 +1,44 @@
+function pulsarboton1() {
+  var x = document.getElementById("pp");
+  if (x.style.display === "none") {
+    x.style.display = '';
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function pulsarboton2() {
+  var x = document.getElementById("p1");
+  if (x.style.display === "none") {
+    x.style.display = '';
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function pulsarboton3() {
+  var x = document.getElementById("p2");
+  if (x.style.display === "none") {
+    x.style.display = '';
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function pulsarboton4() {
+  var x = document.getElementById("p3");
+  if (x.style.display === "none") {
+    x.style.display = '';
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function pulsarboton5() {
+  var x = document.getElementById("p4");
+  if (x.style.display === "none") {
+    x.style.display = '';
+  } else {
+    x.style.display = "none";
+  }
+}
